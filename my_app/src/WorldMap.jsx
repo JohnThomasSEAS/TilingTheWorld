9f91973bc3d66521a78930949c0b44c105bd0c20
John <johnthomas@seas.harvard.edu>
2024-12-04 10:26:29 -0500

    whoops, include process.env

--- my_app/src/WorldMap.jsx
+++ my_app/src/WorldMap.jsx
@@ -88,7 +88,7 @@ const WorldMap = ({ setSelectedCountry, emissionsData }) => {
       )}
       <MapContainer center={[45.505, -0.09]} zoom={2} style={{ height: "100%", width: "100%", borderRadius: "15px" }} ref={mapRef}>
         <TileLayer
-          url={`https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png?api_key=${REACT_APP_STADIA_API_KEY}`}
+          url={`https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png?api_key=${process.env.REACT_APP_STADIA_API_KEY}`}
           attribution='&copy; <a href="https://www.stadiamaps.com/">Stadia Maps</a> contributors &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> contributors &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
         {/* Base GeoJSON layer */}
